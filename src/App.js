c21578b1c7bc98aab96ecdf4b8241fae4aa84c8c
gowthambangaru <gowthambangaru0912@gmail.com>
2024-10-24 12:08:29 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+// App.js
+import React from 'react';
+import Timer from './Timer'; // Import the Timer component
+
+function App() {
+  return (
+    <div className="App">
+      <Timer />
+    </div>
+  );
+}
+
+export default App;
+
